04e3af5e8a2e4bc051c1eef789847d79aa77e812
Jay Rodge <jayrodge@live.com>
2019-01-07 14:54:14 -0600

    Added Predictor

---
+++ predict.py
@@ -0,0 +1,21 @@
+import sys
+import flask
+import pytext
+config_file = sys.argv[1]
+model_file = sys.argv[2]
+
+config = pytext.load_config(config_file)
+predictor = pytext.create_predictor(config, model_file)
+result = predictor({"raw_text": 'THe worst product ever'})
+doc_label_scores_prefix = (
+        'scores:' if any(r.startswith('scores:') for r in result)
+        else 'doc_scores:'
+    )
+
+    # For now let's just output the top document label!
+best_doc_label = max(
+        (label for label in result if label.startswith(doc_label_scores_prefix)),
+        key=lambda label: result[label][0],
+    # Strip the doc label prefix here
+    )[len(doc_label_scores_prefix):]
+print(best_doc_label)
